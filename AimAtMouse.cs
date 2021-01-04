dfbb1a0bd122547a74a5ec79a2f90ded3b4e8456
ch627 <55117528+ch627@users.noreply.github.com>
2021-01-04 15:52:08 -0800

    Aim at the mouse position.
    This function for aiming at the mouse position will come in most handy in shooter games, although could be used for a variety of game genres.

---
+++ AimAtMouse.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimAtMouse : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        faceMouse();
+    }
+
+    void faceMouse()
+    {
+        Vector3 mousePosition = Input.mousePosition; //follow the mouse
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition); // retreive a vector for the object this script is attached to to follow
+        Vector2 direction = new Vector2(mousePosition.x + transform.position.x * -1, mousePosition.y - transform.position.y * -1);
+        transform.right = direction; // point the attached object in a direction specified by the Vector2 variable direction
+    }
+}
