798644033468c86c7ea426a7590204681ba2e6a8
ch627 <55117528+ch627@users.noreply.github.com>
2021-01-04 15:55:06 -0800

    The Aim function applied to a shooter
    In this script, I applied the aim function I made, with a few changes, and applied it to the shooter I'm making.

---
+++ AimWeapon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimWeapon : MonoBehaviour
+{
+    public Transform aimTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        faceMouse();
+    }
+
+    void faceMouse()
+    {
+        Vector3 mouse = Input.mousePosition; //retrieve the mouse position
+        mouse = Camera.main.ScreenToWorldPoint(mouse); // change the position to world space
+        Debug.Log(mouse);
+        Vector3 direction =(mouse - transform.position).normalized;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg; // get an angle in radians, and multiply it to convertt the angle back into degrees
+        aimTransform.eulerAngles = new Vector3(0, 0, angle);
+        Debug.Log(angle);
+    }
+}
